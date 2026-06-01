d3db4f655ff0dc6b3a32926a9522fed879755e02
rholbach-matc <rholbach@madisoncollege.edu>
2026-05-31 23:20:37 -0500

    Add bowl management UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
+import BowlsPage from "./pages/BowlsPage";
+
 function App() {
-  return (
-    <main className="app">
-      <h1>Gizmo</h1>
-      <p>A cozy care tracker for cats who need a little extra love.</p>
-    </main>
-  );
+  return <BowlsPage />;
 }
 
 export default App;
